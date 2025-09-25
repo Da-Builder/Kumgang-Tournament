8d9e04d82ff8744d71dc3ea3c6ce44e12194ec47
Alex Xu <Da.Alexxu@gmail.com>
2025-09-25 01:43:30 +0000

    Add: Login endpoint

--- source/main.py
+++ source/main.py
@@ -1,40 +1,43 @@
-from pathlib import Path
-
+# Import Dependencies
 from boto3 import resource as aws  # type: ignore
 from fastapi import Cookie, FastAPI, HTTPException, Request, status
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+# Application Enviroments
 DATABASE: str = "Test"
 PASSHASH: str = "Test"
 
 
+# Application Setups
 app = FastAPI(default_response_class=HTMLResponse)
 database = aws("dynamodb").Table(DATABASE)
+jinja = Jinja2Templates("template").TemplateResponse
 
-jinja = Jinja2Templates(Path(__file__).parent / "template").TemplateResponse
 
-
+# Application Endpoints
 @app.get("/")
 def home(request: Request, passhash: str = Cookie("")) -> HTMLResponse:
 	if not isinstance(
-		sections := (
-			database.get_item(Key={"title": "All"})
-			.get("Item", {})
-			.get("section")
-		),
+		sections := database.get_item(Key={"name": "All"})
+		.get("Item", {})
+		.get("sections"),
 		list,
 	):
-		raise HTTPException(
-			status.HTTP_500_INTERNAL_SERVER_ERROR,
-			"Internal Error Occurred",
-		)
+		raise HTTPException(status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 	return jinja(
 		request,
 		"home.html",
-		context={
-			"sections": sections,
-			"admin": passhash == PASSHASH,
-		},
+		context={"sections": sections, "admin": verify_passhash(passhash)},
 	)
+
+
+@app.get("/login")
+def login(request: Request) -> HTMLResponse:
+	return jinja(request, "login.html")
+
+
+# Utility Functions
+def verify_passhash(passhash: str) -> bool:
+	return passhash == PASSHASH
